98a3e1e59d8988741369f94671b786f5df7b7b95
threetown <threetowns@163.com>
2017-07-03 16:42:08 +0800

    Add: getMultiEntry function in build utils.js.

--- build/utils.js
+++ build/utils.js
@@ -70,3 +70,28 @@ exports.styleLoaders = function (options) {
   }
   return output
 }
+
+// build/webpack.base.conf.js
+exports.getMultiEntry = function (globPath) {
+  var entries = {},
+    basename, tmp, pathname;
+
+  glob.sync(globPath).forEach(function (entry) {
+    basename = path.basename(entry, path.extname(entry));
+    tmp = entry.split('/').splice(-4);
+
+  var pathsrc = tmp[0]+'/'+tmp[1];
+  if( tmp[0] == 'src' ){
+    pathsrc = tmp[1];
+  }
+  //console.log(pathsrc)
+    pathname = pathsrc + '/' + basename; // 正确输出js和html的路径
+    entries[pathname] = entry;
+
+    //console.log(pathname+'-----------'+entry);
+
+  });
+
+  return entries;
+
+}
