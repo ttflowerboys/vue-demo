a9852884b1ae3b04fc90d20920f02ae77e94a4b3
threetown <threetowns@163.com>
2017-08-23 09:37:12 +0800

    Revert "Add: prerender-spa-plugin"
    This reverts commit 8398d37710022131f5c004891b1f1f725c5e1aef.

--- build/webpack.prod.conf.js
+++ build/webpack.prod.conf.js
@@ -8,7 +8,6 @@ var CopyWebpackPlugin = require('copy-webpack-plugin')
 var HtmlWebpackPlugin = require('html-webpack-plugin')
 var ExtractTextPlugin = require('extract-text-webpack-plugin')
 var OptimizeCSSPlugin = require('optimize-css-assets-webpack-plugin')
-var PrerenderSpaPlugin = require('prerender-spa-plugin')
 
 var env = process.env.NODE_ENV === 'testing'
   ? require('../config/test.env')
@@ -32,10 +31,6 @@ var webpackConfig = merge(baseWebpackConfig, {
     new webpack.DefinePlugin({
       'process.env': env
     }),
-    new PrerenderSpaPlugin(
-      path.resolve(__dirname, '../dist'),
-      [ '/', '/news']
-    ),
     new webpack.optimize.UglifyJsPlugin({
       compress: {
         warnings: false
